010f6fd88e5c2d693f84151d333d4fae8197de34
roman <s91630277@gmail.com>
2018-01-15 00:55:03 +0400

    Added functional tests

---
+++ app/app.ispec.ts
@@ -0,0 +1,31 @@
+import * as express from 'express';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+describe('App test', () => {
+
+    let app: INestApplication;
+    let server: express.Express;
+
+    beforeAll(async () => {
+        const module = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+        server = express();
+        app = module.createNestApplication(server);
+        await app.init();
+    });
+
+    afterAll(() => {
+        app.close();
+    });
+
+    it('/GET /', () => {
+        return request(server)
+            .get('/')
+            .expect(200)
+            .expect('{"data":"Hello World!"}');
+    });
+});
