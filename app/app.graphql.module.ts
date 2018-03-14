1d7b41a76ef4d6c9e088cbc391b0a0ec01c8d981
roman <s91630277@gmail.com>
2018-03-15 00:42:49 +0400

    Renamed module

--- app/app.graphql.module.ts
+++ app/app.graphql.module.ts
@@ -1,6 +1,6 @@
 import { Module, MiddlewaresConsumer, NestModule, RequestMethod } from '@nestjs/common';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
+import { GraphQLFactory, GraphQLModule } from '@nestjs/graphql';
 
 import { AuthorsModule } from './authors/authors.module';
 import { PostsModule } from './posts/posts.module';
@@ -11,18 +11,16 @@ import { PostResolver } from './posts/post.resolver';
     imports: [GraphQLModule, AuthorsModule, PostsModule],
     components: [AuthorResolver, PostResolver],
 })
-export class GraphQLApiModule {
+export class AppGraphQLModule {
 
-    constructor(private readonly graphQLFactory: GraphQLFactory) { }
+    constructor(
+        private readonly graphQLFactory: GraphQLFactory,
+    ) { }
 
     configure(consumer: MiddlewaresConsumer) {
         const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql');
         const schema = this.graphQLFactory.createSchema({ typeDefs });
-
-        consumer
-            .apply(graphiqlExpress({ endpointURL: '/graphql' }))
-            .forRoutes({ path: '/graphiql', method: RequestMethod.GET })
-            .apply(graphqlExpress(req => ({ schema, rootValue: req })))
-            .forRoutes({ path: '/graphql', method: RequestMethod.ALL });
+        consumer.apply(graphiqlExpress({ endpointURL: '/graphql' })).forRoutes({ path: '/graphiql', method: RequestMethod.GET });
+        consumer.apply(graphqlExpress(req => ({ schema, rootValue: req }))).forRoutes({ path: '/graphql', method: RequestMethod.ALL });
     }
 }
