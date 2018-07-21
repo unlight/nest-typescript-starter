1775f91ba74aba5f9a6a89a56eebc8d3a11fd529
roman.vasilev <>
2018-07-21 20:47:31 +0400

    Removed unused file

--- src/core/decorators/access.ts
+++
@@ -1,7 +0,0 @@
-import { ReflectMetadata } from '@nestjs/common';
-
-// const editResource = ({user, resource}) => user.id === resource.insertUserId || user.role === 'ADMIN' ;
-
-export function Access(checkAccess: (...args) => boolean) {
-    return ReflectMetadata('access', checkAccess);
-}
