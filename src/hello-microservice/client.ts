b86dd0a132674498b216d5d9841b18d057f7589a
roman.vasilev <>
2018-08-29 21:06:59 +0400

    fixed lint warning in client.ts

--- src/hello-microservice/client.ts
+++ src/hello-microservice/client.ts
@@ -1,5 +1,5 @@
 import { Socket } from 'net';
-const JsonSocket = require('json-socket');
+const JsonSocket = require('json-socket'); // eslint-disable-line tslint2/config
 
 const port = 43210; // The same port that the server is listening on
 const host = '127.0.0.1';
@@ -7,7 +7,7 @@ const socket: Socket = new JsonSocket(new Socket()); // Decorate a standard net.
 socket.connect(port, host);
 socket.on('connect', () => { // Don't send until we're connected
     console.log('connected');
-    <any>socket.sendMessage({ pattern: 'service1', data: {a: 1, b: 2 }});
+    (<any>socket).sendMessage({ pattern: 'service1', data: {a: 1, b: 2 }});
     socket.on('message', (message) => {
         console.log('message', message);
         // console.log('The result is: '+message.result);
