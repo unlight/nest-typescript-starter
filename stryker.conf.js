41fe3abcb457b3605474b1494cddbb695e76e688
roman.vasilev <>
2018-09-05 12:58:44 +0400

    mutation test for mocha

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,11 +4,22 @@ module.exports = function(config) {
         files: [
             'src/**/*.ts',
             'src/**/*.graphql',
+            'package.json',
         ],
-        testRunner: 'jest',
+        testRunner: 'mocha',
         mutator: 'typescript',
         transpilers: [],
         reporters: ['html', 'clear-text', 'progress'],
+        testFramework: 'mocha',
+        mochaOptions: {
+            files: [ 'src/**/*.spec.ts' ],
+            // opts: 'path/to/mocha.opts',
+            // ui: 'bdd',
+            timeout: 8000,
+            require: ['ts-node/register/transpile-only'],
+            asyncOnly: false,
+        },
+        packageManager: 'npm',
         coverageAnalysis: 'off',
         tsconfigFile: 'tsconfig.json',
         mutate: [
